e1e026c2e7bd9aebf812327dedfd3413da0b10f3
Baraded <borodeda@gmail.com>
2025-02-24 23:03:45 +0300

    Commite first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.Date;
+
+public class Product {
+    String name, manufacturer, country;
+    Date date;
+    double price;
+    boolean status;
+
+    public Product()
+}
