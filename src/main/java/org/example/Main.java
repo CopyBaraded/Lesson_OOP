0156f1f69cf2a0d383dfe37e6ae9b65092b61ddf
Baraded <borodeda@gmail.com>
2025-02-25 23:44:54 +0300

    Без вывода не полноценно

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,15 +15,19 @@ productsArray[4] = new Product(...);
 
 public class Main {
     public static void main(String[] args) {
-        Product[] productsArray = new Product[5];
+//        Product[] productsArray = new Product[5];
+//
+//        productsArray[0] = new Product("Sums S25 U", "Sum corp.", "Korea", "13.01.2025", 2500, false);
+//        productsArray[1] = new Product("Google Pixel 6", "Hon Hai Precision Industry Co.", "Taiwan", "15.03.2020", 1100, true);
+//        productsArray[2] = new Product("Google Pixel 7", "Hon Hai Precision Industry Co.", "Taiwan", "25.12.2022", 2100, true);
+//        productsArray[3] = new Product("Google Pixel 8", "Hon Hai Precision Industry Co.", "Taiwan", "03.09.2024", 2100, true);
+//        productsArray[4] = new Product("Nokia 3310", "Human Mobile Devices", "Finland", "26.02.2017", 100, false);
+//
+//
+//        productsArray[2].productInfo();
+        Park imGorkogo = new Park("им. М.Горького");
 
-        productsArray[0] = new Product("Sums S25 U", "Sum corp.", "Korea", "13.01.2025", 2500, false);
-        productsArray[1] = new Product("Google Pixel 6", "Hon Hai Precision Industry Co.", "Taiwan", "15.03.2020", 1100, true);
-        productsArray[2] = new Product("Google Pixel 7", "Hon Hai Precision Industry Co.", "Taiwan", "25.12.2022", 2100, true);
-        productsArray[3] = new Product("Google Pixel 8", "Hon Hai Precision Industry Co.", "Taiwan", "03.09.2024", 2100, true);
-        productsArray[4] = new Product("Nokia 3310", "Human Mobile Devices", "Finland", "26.02.2017", 100, false);
+        Park.SaveInfoPark haliGali = imGorkogo.new SaveInfoPark("Хали-Гали ", "с 8:00 до 21.00 ", 13);
 
-
-        productsArray[2].productInfo();
     }
 }
