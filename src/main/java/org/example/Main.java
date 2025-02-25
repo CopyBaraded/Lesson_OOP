762aa99b822f2a58e424263e3c60492299a1d613
Baraded <borodeda@gmail.com>
2025-02-25 12:27:47 +0300

    строку в дату не получилось, вернуться

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
+/*
+Task 4 Создать массив из 5 товаров.
+Пример:
+вначале объявляем массив объектов
+Product[] productsArray = new Product[5];
+потом для каждой ячейки массива задаем объект
+productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025",
+"Samsung Corp.", "Korea", 5599, true);
+productsArray[1] = new Product(...);
+productsArray[4] = new Product(...);
+ */
 
-import java.text.SimpleDateFormat;
+
 
 public class Main {
     public static void main(String[] args) {
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH");
+
 
 
         Product prod = new Product("Sums S25 U", "Sum corp.", "Asia", "13.01.2025", 2500, false);
